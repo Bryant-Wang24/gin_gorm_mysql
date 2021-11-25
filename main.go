8bef8967146eebbd682319d0b8d5cb679f3a3109
Bryant-Wang24 <485969746@qq.com>
2021-11-25 08:43:58 +0800

    gin第一个程序

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+    // 1.创建路由
+   r := gin.Default()
+   // 2.绑定路由规则，执行的函数
+   // gin.Context，封装了request和response
+   r.GET("/", func(c *gin.Context) {
+      c.String(http.StatusOK, "hello World!")
+   })
+   r.GET("/user/:name/*action",func(c *gin.Context) {
+      name:=c.Param("name")
+      action:=c.Param("action")
+      // 截取
+      action = strings.Trim(action,"/")
+      c.String(http.StatusOK,name + " is " + action)
+   })
+   // 3.监听端口，默认在8080
+   // Run("里面不指定端口号默认为8080") 
+   r.Run(":8000")
+}
